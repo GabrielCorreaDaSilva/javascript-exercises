1b7147905e36d260717894f4bbd9ca9abf0c9aa7
GabrielCSilva <211619696+GabrielCorreaDaSilva@users.noreply.github.com>
2025-09-03 03:11:04 -0300

    Solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    if(n == 0) return 0;
 
-};
+    let arr = [0, 1];
+    for(let i = 2; i <= n; i++) {
+            arr.push(arr[0] + arr[1]);
+            arr.shift();
+        }
+    return arr[arr.length - 1];
+    }
+
 
 // Do not edit below this line
 module.exports = fibonacci;
