40a028ea4bf90fb404be0e2b236bb9d268625902
GabrielCSilva <211619696+GabrielCorreaDaSilva@users.noreply.github.com>
2025-09-06 01:58:00 -0300

    Solve exercises 15-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+// const findTheOldest = function(arr) {
+//     arr.sort((a, b)=> {
+//         a.yearOfDeath = a.yearOfDeath || new Date().getFullYear();
+//         b.yearOfDeath = b.yearOfDeath || new Date().getFullYear();
+//         return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+//     });
+//     return arr[0];
+// };
+function getAge(birth, death) {
+    death = death || new Date().getFullYear();
+    return death - birth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current)=>{
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
